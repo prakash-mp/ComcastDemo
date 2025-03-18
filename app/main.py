611f0217fec09285cdce8b575f386e319aa3eaf3
Prakash <prakash.c@prodapt.com>
2025-03-18 19:30:48 +0530

    Update CORS middleware

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.include_router(transaction.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
