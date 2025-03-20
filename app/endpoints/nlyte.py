9744799d952b829245fbe1a51574eb01aee0e9fa
Prakash <prakash.c@prodapt.com>
2025-03-20 16:46:37 +0530

    Update nlte update endpoints

--- app/endpoints/nlyte.py
+++ app/endpoints/nlyte.py
@@ -87,11 +87,48 @@ def update_nlyte_hub(
             content={
                 "code": 404,
                 "status": "Not found",
-                "message": "Error",
+                "message": f"No record found with hub_id {hub_id}",
             },
         )
 
-    # tbi
+    db_obj.dc_id = data_in.dc_id
+    db_obj.name = data_in.name
+    db_obj.address = data_in.location.address
+    db_obj.latitude = data_in.location.latitude
+    db_obj.longitude = data_in.location.longitude
+    db_obj.power_capacity_kw = data_in.power_capacity_kw
+    db_obj.available_power_capacity_kw = data_in.available_power_capacity_kw
+    db_obj.total_racks = data_in.total_racks
+    db_obj.available_racks = data_in.available_racks
+    db_obj.asset_id = data_in.asset.asset_id
+    db_obj.asset_type = data_in.asset.asset_type
+    db_obj.manufacturer = data_in.asset.manufacturer
+    db_obj.model = data_in.asset.model
+    db_obj.asset_status = data_in.asset.asset_status
+    db_obj.asset_location = data_in.asset.asset_location
+    db_obj.power_consumption_kw = data_in.energy_usage.power_consumption_kw
+    db_obj.peak_usage_kw = data_in.energy_usage.peak_usage_kw
+    db_obj.average_usage_kw = data_in.energy_usage.average_usage_kw
+    db_obj.energy_efficiency_rating = data_in.energy_usage.energy_efficiency_rating
+    db_obj.cooling_capacity_ton = data_in.cooling_unit.cooling_capacity_ton
+    db_obj.current_temperature_celsius = (
+        data_in.cooling_unit.current_temperature_celsius
+    )
+    db_obj.cooling_unit_status = data_in.cooling_unit.cooling_unit_status
+    db_obj.cooling_unit_location = data_in.cooling_unit.cooling_unit_location
+
+    db.commit()
+
+    log.info(f"nlyte data updated for the hub id {hub_id}")
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "code": 200,
+            "status": "OK",
+            "message": jsonable_encoder(db_obj.to_schema()),
+        },
+    )
 
 
 @router.delete("/nlyte/hubs/{hub_id}")
